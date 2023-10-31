f893ea6b1b08b4e4fdb7904c919a1242df3f8a69
Nelson <n00gomez00@gmail.com>
2023-10-30 22:52:52 -0600

    add login functions

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,4 +1,6 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth';
 
 
 export const Navbar = () => {
@@ -11,7 +13,7 @@ export const Navbar = () => {
        });
     }
 
-
+    const {user} = useContext(AuthContext)
 
 
     return (
@@ -54,7 +56,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                    <span className='nav-item nav-link text-primary'>
-                    Nelson
+                    {user?.name}
                    </span>
                    <button className='nav-item nav-link btnS'
                    onClick={onLogout}>
